7bf8dce65460a687008c2c0e4bde36f90bc30f7a
Arno Schamber <arno.schamber@deutsche-boerse.com>
2024-09-09 18:17:02 +0200

    Disableing  ruby and perl

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -5,6 +5,8 @@ if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath })
 end
 vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
+vim.g.loaded_perl_provider = 0
+vim.g.loaded_ruby_provider = 0
 
 require("lazy").setup({
   spec = {
